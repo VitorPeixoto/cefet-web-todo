c8892e1993c265b6a08eea4f6d1bfcde003df63a
Vitor Peixoto <vitorgslpeixoto@gmail.com>
2021-07-16 20:02:56 -0300

    Exercício 1: Carregar tarefas existentes1~

--- tarefas.js
+++ tarefas.js
@@ -1,4 +1,24 @@
 const tarefas = [
     { nome: "Formar logo por favor", categoria: "estudos", realizada: false },
     { nome: "Me tornar uma Lenda Pirata no Sea of Thieves", categoria: "lazer", realizada: false }
-]
+];
+
+const taskListElement = document.querySelector('ul#lista-tarefas');
+
+const insereTarefaNaPagina = (tarefa) => {
+    const taskElement = document.createElement('li');
+    
+    taskElement.classList.add('item-tarefa');
+    taskElement.classList.add(`categoria-${tarefa.categoria}`);
+    if(tarefa.realizada)
+        taskElement.classList.add('marcado');    
+    
+    const textElement = document.createElement('p');
+    textElement.innerHTML = tarefa.nome;
+
+    taskElement.appendChild(textElement);
+    taskListElement.appendChild(taskElement);
+}
+
+taskListElement.innerHTML = '';
+tarefas.forEach( insereTarefaNaPagina );
