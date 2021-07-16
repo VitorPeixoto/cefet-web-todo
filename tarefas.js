c55d0db8d6224600462c2e386cebc207cb17f5ac
Vitor Peixoto <vitorgslpeixoto@gmail.com>
2021-07-16 19:20:29 -0300

    Exercício 0: Representar tarefas

---
+++ tarefas.js
@@ -0,0 +1,4 @@
+const tarefas = [
+    { nome: "Formar logo por favor", categoria: "estudos", realizada: false },
+    { nome: "Me tornar uma Lenda Pirata no Sea of Thieves", categoria: "lazer", realizada: false }
+]
